56bde159f260fca6d39f304f1cf92218e8e79f2f
agichimu <alexandergichimu98@gmail.com>
2023-10-02 12:54:28 +0300

    first commit

---
+++ Helloworld.py
@@ -0,0 +1,37 @@
+# //What does this command line print?
+
+print(f"{98} Battery street, {'San Francisco'}")
+
+# //What does this command line print?
+
+a = "Python is cool"
+print(a[4])
+
+# //What does this command line print?
+
+a = "Python is cool"
+print(a[7:-5])
+
+# //What does this command line print?
+
+a = "Python is cool"
+print(a[0:6])
+
+# //What does this command line print?
+
+a = "Python is cool"
+print(a[7:])
+
+# //What does this command line print?
+
+print("Holberton school")
+
+# //What does this command line print?
+
+a = "Python is cool"
+print(a[:6])
+
+# //What does this command line print?
+
+a = "Python is cool"
+print(a[-2])
